34a27059b8f14d49e8476829df7000802adfe8e8
brread <63481332+brread@users.noreply.github.com>
2022-04-13 16:09:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ let sdate = new Date().getTime();
 let timestamp;
 let spacepressed;
 let timep;
-let menu;
-let solvestatus = false;
-const ttext = document.getElementById('timer')
-const solves = document.getElementById('solves')
+let settingsmenu;
+let solvesmenu;
+let cube;
+let settingsstatus = false;
+let solvesstatus = false;
+const ttext = document.getElementById('timer');
+const settings = document.getElementById('settings');
+const solves = document.getElementById('solves');
 
 setInterval(function () {
     checks()
@@ -41,36 +45,118 @@ window.addEventListener('keyup', function (key) {
 })
 
 function checks() {
-    if (state == 'ready') {
-        ttext.style.color = "limegreen";
-    } else if (state == 'starting') {
-        timer.style.color = "red";
-        timestamp = new Date().getTime();
-        timestamp = (((timestamp - timep)/1000).toFixed(2));
-        if (timestamp >= 0.4) {
-            state = 'ready';
+    if (!solvesstatus && !settingsstatus) {
+        if (state == 'ready') {
+            ttext.style.color = "limegreen";
+        } else if (state == 'starting') {
+            ttext.style.color = "red";
+            timestamp = new Date().getTime();
+            timestamp = (((timestamp - timep)/1000).toFixed(2));
+            if (timestamp >= 0.4) {
+                state = 'ready';
+            }
+        } else if (state == 'started') {
+            timep = new Date().getTime();
+            ttext.style.color = 'black';
+            ttext.innerText = ((timep - timestamp)/1000).toFixed(2);
+        } else if (state == 'not initialized') {
+            ttext.style.color = 'black';
         }
-    } else if (state == 'started') {
-        timep = new Date().getTime();
-        ttext.style.color = 'black';
-        ttext.innerText = ((timep - timestamp)/1000).toFixed(2);
-    } else if (state == 'not initialized') {
-        ttext.style.color = 'black';
     }
 }
 
+settings.addEventListener('click', function () {
+    if (!settingsstatus) {
+        settingsstatus = true;
+        settingsmenu = document.createElement('div');
+        let bar = document.createElement('div');
+        let choose = document.createElement('select');
+        let op1 = document.createElement('option');
+        op1.value = '1';
+        op1.innerText = '3x3';
+        choose.style.position = 'absolute';
+        choose.style.fontSize = '16px';
+        choose.style.cursor = 'pointer';
+        choose.style.appearance = 'none';
+        choose.style.left = '50%';
+        choose.style.top = '50%';
+        choose.style.transform = 'translate(-50%, -50%)'
+        choose.style.width = "40px";
+        choose.style.height = "20px";
+        choose.appendChild(op1);
+        bar.appendChild(choose);
+        bar.style.width = '100%';
+        bar.style.height = '10%';
+        bar.style.margin = '0';
+        bar.style.top = '0';
+        bar.style.color = "#ffebc4";
+        bar.style.borderBottom = "2px solid black";
+        bar.style.position = "relative";
+        settingsmenu.style.position = 'absolute';
+        settingsmenu.style.width = "50vw";
+        settingsmenu.style.height = "65vh";
+        settingsmenu.style.backgroundColor = '#ffebc4';
+        settingsmenu.style.border = "2px solid black";
+        settingsmenu.style.top = "50%";
+        settingsmenu.style.left = "50%";
+        settingsmenu.style.transform = "translate(-50%, -50%)";
+        settingsmenu.appendChild(bar);
+        document.body.appendChild(settingsmenu);
+    } else if (settingsstatus) {
+        settingsstatus = false;
+        document.body.removeChild(settingsmenu);
+    }
+})
+
 solves.addEventListener('click', function () {
-    if (!solvestatus) {
-        solvestatus = true;
-        menu = document.createElement('div');
-        menu.style.position = 'absolute';
-        menu.style.width = "50vw";
-        menu.style.height = "50vh";
-        menu.style.backgroundColor = '#ffebc4';
-        menu.style.border = "2px solid black";
-        document.body.appendChild(menu);
-    } else {
-        solvestatus = false;
-        document.body.removeChild(menu);
+    if (!solvesstatus && !settingsstatus) {
+        solvesstatus = true;
+        solvesmenu = document.createElement('div');
+        let bar = document.createElement('div');
+        let choose = document.createElement('select');
+        let op1 = document.createElement('option');
+        let session = document.createElement('span');
+        op1.value = '1';
+        op1.innerText = '3x3';
+        choose.style.position = 'absolute';
+        choose.style.fontSize = '16px';
+        choose.style.cursor = 'pointer';
+        choose.style.appearance = 'none';
+        choose.style.left = '50%';
+        choose.style.top = '50%';
+        choose.style.transform = 'translate(-50%, -50%)'
+        choose.style.width = "40px";
+        choose.style.height = "20px";
+        choose.appendChild(op1);
+        session.style.position = 'relative';
+        session.style.fontSize = '16px';
+        session.style.margin = 'auto';
+        session.style.marginRight = "55%";
+        session.innerText = "Session";
+        session.style.color = "black";
+        session.style.textAlign = 'right';
+        session.style.display = 'block';
+        bar.appendChild(session)
+        bar.appendChild(choose);
+        bar.style.width = '100%';
+        bar.style.height = '10%';
+        bar.style.margin = '0';
+        bar.style.top = '0';
+        bar.style.color = "#ffebc4";
+        bar.style.borderBottom = "2px solid black";
+        bar.style.position = "relative";
+        solvesmenu.style.position = 'absolute';
+        solvesmenu.style.width = "50vw";
+        solvesmenu.style.height = "65vh";
+        solvesmenu.style.backgroundColor = '#ffebc4';
+        solvesmenu.style.border = "2px solid black";
+        solvesmenu.style.top = "50%";
+        solvesmenu.style.left = "50%";
+        solvesmenu.style.transform = "translate(-50%, -50%)";
+        solvesmenu.appendChild(bar);
+        document.body.appendChild(solvesmenu);
+    } else if (solvesstatus) {
+        solvesstatus = false;
+        document.body.removeChild(solvesmenu);
     }
 })
